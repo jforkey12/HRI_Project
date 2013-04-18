67a44c434b547747f36c6bb132c727e118c3bf17
James Forkey <jmforkey@wpi.edu>
2013-04-18 17:38:49 -0400

    Robot updates

--- src/main/java/rosdisco/Server.java
+++ src/main/java/rosdisco/Server.java
@@ -12,6 +12,7 @@ import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
 import org.ros.node.service.ServiceResponseBuilder;
 import org.ros.node.service.ServiceServer;
+import java.io.IOException;
 
 public class Server extends AbstractNodeMain {
 	
@@ -28,42 +29,44 @@ public class Server extends AbstractNodeMain {
           public void build(rosd_messages.RosDRequest request, rosd_messages.RosDResponse response) {
     		System.out.println("DEBUG: START OF BUILD MESSAGE");
 		String command = buildCommand(request.getTask());
-
+            long responseNum = 0;
     		try {
     			System.out.println("DEBUG: SLEEP TO SIMULATE ROBOT MOVEMENT");
-    			  Thread.sleep(10000);
 			if(!command.equals("")) {
 				executeCommand(command);
+
 			}
-    			System.out.println("DEBUG: BEFORE SETTING RESPONSE MESSAGE");
+    			Thread.sleep(5000);
+                System.out.println("DEBUG: BEFORE SETTING RESPONSE MESSAGE");
 
 			if(command.equals(command)){
-				long responseNum = 1;
+				responseNum = 1;
 			}
-
             	  response.setResponseMessage(responseNum);
             	  System.out.println("DEBUG: AFTER SETTING RESPONSE MESSAGE");
               }
               catch (Exception e){}
           } 
 	
-	 public static String buildCommand(String line) {
+	 public String buildCommand(String line) {
 		String command = "Do not know how to execute that command yet...";
 		
-		if(line.equalsIgnoreCase("forward")) {
-			System.out.println("moving forward...");
+		if(line.equalsIgnoreCase("SetDinnerTable()")) {
+			System.out.println("Setting Dinner Table.. non-primitive.. do nothing...");
+		}
+		else if(line.equalsIgnoreCase("SetPlace()")) {
+			System.out.println("Setting Place.. non-primitive.. do nothing...");
+		}
+		else if(line.equalsIgnoreCase("SetNapkin()")) {
+			System.out.println("Setting Napkin.. robot moves forward...");
 			command = "rostopic pub /turtle1/command_velocity turtlesim/Velocity -- 2.0 0.0";
-		}
-		else if(line.equalsIgnoreCase("left")) {
-
-		}
-		else if(line.equalsIgnoreCase("right")) {
 
 		}
 		else if(line.equalsIgnoreCase("SetCloth()")) {
-			System.out.println("Setting cloth...");
-			command = "rostopic pub /turtle1/command_velocity turtlesim/Velocity -- 2.0 0.0";
+			System.out.println("Setting cloth...Robot moves in a circle...");
+			command = "rostopic pub /turtle1/command_velocity turtlesim/Velocity -- 5.0 3.8";
 		}
+		
 		else if(line.equalsIgnoreCase("quit")) {
 			System.out.println("quitting...");
 			return "";	
@@ -71,7 +74,7 @@ public class Server extends AbstractNodeMain {
 		return command;
 	 }
 
-	public static void executeCommand(String command) {
+	public void executeCommand(String command) {
 		Runtime r = Runtime.getRuntime();
 		try {
 			Process p = r.exec(command);
