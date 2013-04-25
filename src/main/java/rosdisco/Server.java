946ef956b44bade1e1a3240a1a9cf88b185bdb7b
Trevor Hodde <trevorhodde@ubuntu.(none)>
2013-04-24 18:18:38 -0700

    Adding request handling to server

--- src/main/java/rosdisco/Server.java
+++ src/main/java/rosdisco/Server.java
@@ -25,18 +25,61 @@ public class Server extends AbstractNodeMain {
     connectedNode.newServiceServer("rosdisco", rosd_messages.RosD._TYPE,
         new ServiceResponseBuilder<rosd_messages.RosDRequest, rosd_messages.RosDResponse>() {
     	@Override
-          public void
-             build(rosd_messages.RosDRequest request, rosd_messages.RosDResponse response) {
+          public void build(rosd_messages.RosDRequest request, rosd_messages.RosDResponse response) {
     		System.out.println("DEBUG: START OF BUILD MESSAGE");
+		String command = buildCommand(request.getTask());
+
     		try {
     			System.out.println("DEBUG: SLEEP TO SIMULATE ROBOT MOVEMENT");
     			  Thread.sleep(10000);
-    			  System.out.println("DEBUG: BEFORE SETTING RESPONSE MESSAGE");
-            	  response.setResponseMessage( 123/*request.getTask()*/);
+			if(!command.equals("")) {
+				executeCommand(command);
+			}
+    			System.out.println("DEBUG: BEFORE SETTING RESPONSE MESSAGE");
+
+			if(command.equals(command)){
+				long responseNum = 1;
+			}
+
+            	  response.setResponseMessage(responseNum);
             	  System.out.println("DEBUG: AFTER SETTING RESPONSE MESSAGE");
               }
               catch (Exception e){}
           } 
+	
+	 public static String buildCommand(String line) {
+		String command = "Do not know how to execute that command yet...";
+		
+		if(line.equalsIgnoreCase("forward")) {
+			System.out.println("moving forward...");
+			command = "rostopic pub /turtle1/command_velocity turtlesim/Velocity -- 2.0 0.0";
+		}
+		else if(line.equalsIgnoreCase("left")) {
+
+		}
+		else if(line.equalsIgnoreCase("right")) {
+
+		}
+		else if(line.equalsIgnoreCase("SetCloth()")) {
+			System.out.println("Setting cloth...");
+			command = "rostopic pub /turtle1/command_velocity turtlesim/Velocity -- 2.0 0.0";
+		}
+		else if(line.equalsIgnoreCase("quit")) {
+			System.out.println("quitting...");
+			return "";	
+		}
+		return command;
+	 }
+
+	public static void executeCommand(String command) {
+		Runtime r = Runtime.getRuntime();
+		try {
+			Process p = r.exec(command);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
         });
   }
 }
